237964898d90f8a04bc68a5496731af43c1843e6
maurobiazutti <mauro_biazutti@hotmail.com>
2022-07-08 14:33:46 -0300

    Criando gem exercicios_ruby

--- exercicios/exe_02.rb
+++
@@ -1,9 +0,0 @@
-# Metodo auxiliar
-# Dado dois inteiros x e y inverter os dados de x pra y
-x1 = 10
-y1 = 20
-
-auxi = x1
-x1 = y1
-y1 = auxi
-puts "x1 = #{x1}, y1 = #{y1}"
--- exercicios/exe_08.rb
+++
@@ -1,9 +0,0 @@
-# Calcular meida de notas aluno n1 = peso 2, n2 = peso 3 e n3 = peso 5
-puts "Digete nota 1"
-n1 = gets.chomp.to_f
-puts "Digete nota 2"
-n2 = gets.chomp.to_f
-puts "Digete nota 3"
-n3 = gets.chomp.to_f
-media = (n1 * 2) + (n2 * 3) + (n3 * 5) / 10
-puts "#{media.round 2}"
